d2185125849fa7ee3abe99af0de79ecb701dab87
Martin Cook <martin.cook@standardbank.co.za>
2023-11-29 22:56:16 +0200

    Improved random player code: no longer needs random attempts

--- random_player_plus.py
+++ random_player_plus.py
@@ -16,52 +16,44 @@ class RandomPlayerPlus (Player):
         rows = board.shape[0]
         cols = board.shape[1]
 
+        # get a list of positions of tiles with a count > 1
+        non_zero_tiles = np.argwhere(board != 0)
+        multi_tiles = np.argwhere(np.abs(board) > 1)
+
+        options = ['plant']
+        if len(non_zero_tiles) > 0:
+            options.append('fertilise')
         if moves_remaining > 1:
-            options = ['fertilise', 'plant', 'colonise', 'spray', 'bomb']
-        else:
-            options = ['fertilise', 'plant']
+            options.extend(['spray', 'bomb'])
+            if len(multi_tiles) > 0:
+                options.append('colonise')
 
         while True:
             move = random.choice(options)
             if move == 'fertilise':
-                for i in range(attempts):
-
-                    row = random.randint(0, rows - 1)
-                    col = random.randint(0, cols - 1)
-                    if board[row][col] * self.player > 0:
-                        return move, [row, col]
+                pos = random.choice(non_zero_tiles)
+                return move, pos
 
             elif move == 'plant':
-                for i in range(attempts):
-                    row = random.randint(0, rows - 1)
-                    col = random.randint(0, cols - 1)
-                    if board[row][col] == 0:
-                        for delta_r, delta_c in [
-                            (0, -1), (0, 1), (1, 0), (-1, 0)
-                        ]:
-                            if 0 <= row + delta_r < rows \
-                                    and 0 <= col + delta_c < cols:
-                                if board[row + delta_r][col + delta_c] \
-                                        * self.player > 0:
-                                    return move, [row, col]
+                row, col = random.choice(non_zero_tiles)
+                for delta_r, delta_c in [
+                    (0, -1), (0, 1), (1, 0), (-1, 0)
+                ]:
+                    test_row = row + delta_r
+                    test_col = col + delta_c
+                    if 0 <= test_row < rows and 0 <= test_col < cols:
+                        if board[test_row][test_col] == 0:
+                            return move, [test_row, test_col]
 
             elif move == 'colonise':
-                found_source = False
-                source_row = -1
-                source_col = -1
+                source_row, source_col = random.choice(multi_tiles)
                 for i in range(attempts):
-                    source_col = random.randint(0, cols - 1)
-                    source_row = random.randint(0, rows - 1)
-                    if board[source_row][source_col] * self.player > 1:
-                        found_source = True
-                        break
-
-                if found_source:
-                    for i in range(attempts):
-                        row = random.randint(0, rows - 1)
-                        col = random.randint(0, cols - 1)
-                        if board[row][col] == 0:
-                            return move, [row, col, source_row, source_col]
+                    zero_tiles = np.argwhere(board == 0)
+                    if len(zero_tiles) == 0:
+                        continue
+                    else:
+                        row, col = random.choice(zero_tiles)
+                        return move, [row, col, source_row, source_col]
 
             elif move == 'spray':
                 row = random.randint(0, rows - 1)
