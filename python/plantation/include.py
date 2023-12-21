bf297802e9478932cbeabc9ea9721e579c277cbf
eyevzz <norman@jetprotocol.io>
2023-12-21 10:55:14 +0000

    Adds packaging boilerplate

--- python/plantation/include.py
+++ python/plantation/include.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def get_player_restricted_board(board: np.ndarray, player) -> np.array:
+def get_player_restricted_board(board: np.ndarray, player) -> np.ndarray:
     """ Returns a copy of the board with only the player's tiles. """
 
     return board * (board * player > 0)
